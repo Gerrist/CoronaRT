b58e8a5e87812ba09d24bf6cae50e9f96902f19f
Gerrist <35576282+Gerrist@users.noreply.github.com>
2020-03-22 23:15:31 +0100

    commit

--- index.js
+++ index.js
@@ -15,16 +15,15 @@ const server = http.createServer(app);
 
 var wss = new WebSocketServer({server: server});
 
+let proj = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs";
+
 wss.on('connection', (ws) => {
     ws.on('message', (message) => {
         try {
             let m = JSON.parse(message);
-            console.log(m);
-            if ('lat' in m) {
-                m['RD-X'] = proj4("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs", [m['lat'], m['lon']])[0];
-            }
-            if ('lon' in m) {
-                m['RD-Y'] = proj4("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs", [m['lat'], m['lon']])[1];
+            if ('lat' in m && 'lon' in m) {
+                m['RD-X'] = proj4(proj, [m['lat'], m['lon']])[0];
+                m['RD-Y'] = proj4(proj, [m['lat'], m['lon']])[1];
             }
             wss.clients.forEach(function each(client) {
                 client.send(JSON.stringify(m));
@@ -51,12 +50,11 @@ app.get('/api/lladder.json', (req, res) => {
             let c = proj4('+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs')
                 .inverse([data.scheduledStopPoints[k].x, data.scheduledStopPoints[k].y]);
 
-            const texelRD = new Cartesian(data.scheduledStopPoints[k].x, data.scheduledStopPoints[k].y, 1.0000);
-            const texelETRS = Transform.rdnap2etrs(texelRD);
-            // console.log(texelETRS);
+            const RD = new Cartesian(data.scheduledStopPoints[k].x, data.scheduledStopPoints[k].y, 1.0000);
+            const ETRS = Transform.rdnap2etrs(RD);
 
-            data.scheduledStopPoints[k].lat = texelETRS.phi;
-            data.scheduledStopPoints[k].lon = texelETRS.lambda;
+            data.scheduledStopPoints[k].lat = ETRS.phi;
+            data.scheduledStopPoints[k].lon = ETRS.lambda;
         });
 
         data = JSON.stringify(data);
